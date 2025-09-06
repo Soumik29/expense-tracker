d105f4858bc72addba52bb278f235a8ba1a97342
Soumik Mitra <soumikdeb2@gmail.com>
2025-09-06 01:59:54 -0400

    I was trying out to create a modalForm when a user clicks on the edit icon. I was successful in creating a modal form but it is not working as expected.

--- src/ExpenseTracker.tsx
+++ src/ExpenseTracker.tsx
@@ -7,6 +7,7 @@ import ModalFormExpense from "./ModalFormExpense";
 const ExpenseTracker = () => {
   const [expense, setExpense] = useState<Expense[]>([]);
   const [editExpense, setEditExpense] = useState<Expense | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const addExpense = (expense: Expense) => {
     setExpense((prev) => [...prev, expense]);
   };
@@ -14,31 +15,27 @@ const ExpenseTracker = () => {
     setExpense((prev) => prev.filter((expense) => expense.id !== id));
   };
 
-  const updateExpense = (updatedExpense: Expense) => {
-    setExpense((prev) =>
-      prev.map((exp) => (exp.id === updatedExpense.id ? updatedExpense : exp))
-    );
-    setEditExpense(updatedExpense);
-  };
-
-  const modalClose = () => {
-    setEditExpense(null);
-  };
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6 items-stretch">
       <AddExpenseForm onAddExpense={addExpense} />
       <div className="h-full w-full max-w-md mx-auto space-y-4">
         <div className="overflow-y-auto max-h-115">
-          <ExpenseCard expenseList={expense} onDeleteExpense={deleteExpense} onEditExpense={setEditExpense}/>
+          <ExpenseCard
+            expenseList={expense}
+            onDeleteExpense={deleteExpense}
+            onEditExpense={setEditExpense}
+          />
         </div>
         {expense.length !== 0 ? <TotalExpense totalExp={expense} /> : null}
       </div>
-      <ModalFormExpense 
-        expense = {editExpense}
-        onClose = {modalClose}
-        onSave = {updateExpense}
+      {editExpense ? (
+        <ModalFormExpense
+          open={isOpen}
+          close={setIsOpen}
+          exp={editExpense}
+          saveExpense={setExpense}
         />
+      ) : null}
     </div>
   );
 };
--- src/ModalFormExpense.tsx
+++ src/ModalFormExpense.tsx
@@ -1,161 +1,130 @@
-import { type Expense } from "./types";
-import { type ExpenseCategory } from "./AddExpenseForm";
 import {
+  Button,
+  Description,
   Dialog,
-  DialogBackdrop,
   DialogPanel,
   DialogTitle,
+  Field,
+  Input,
+  Label,
+  Select,
+  Textarea,
 } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import type { Expense } from "./types";
+import { useState } from "react";
+import type { ExpenseCategory } from "./AddExpenseForm";
 
-type editExpenseProps = {
-  expense: Expense | null;
-  onClose: () => void;
-  onSave: (updatedExpense: Expense) => void;
-};
+interface modalClose {
+  close: (boo: boolean) => void;
+  open: boolean;
+  exp: Expense;
+  saveExpense: (expe: Expense[]) => void;
+}
 
-const ModalFormExpense = ({ expense, onClose, onSave }: editExpenseProps) => {
-  if (!expense) return null;
+const ModalFormExpense = ({ close, open, exp, saveExpense }: modalClose) => {
+  const [tempExpValues, setTempExpValues] = useState<Expense>(exp);
 
+  // const handleSaveExpense = (id: number) => {
+
+  // }
   return (
-    <div>
-      <Dialog open={!!expense} onClose={onClose} className="relative z-10">
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-gray-900/50 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
-        />
-
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <DialogPanel
-              transition
-              className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
-            >
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <DialogTitle
-                  as="h3"
-                  className="text-base font-semibold text-white"
-                >
-                  Update Expense
-                </DialogTitle>
-              </div>
-              <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="amount"
-                    className="block text-sm/6 font-medium text-white"
-                  >
-                    Amount
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="amount"
-                      name="ExpenseAmount"
-                      type="number"
-                      placeholder="Amount to be changed"
-                      className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
-                      value={expense.amount}
-                      onChange={(e) =>
-                        onSave({
-                          ...expense,
-                          amount: parseFloat(e.target.value),
-                        })
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="description"
-                    className="block text-sm/6 font-medium text-white"
-                  >
-                    Description
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="description"
-                      name="ExpenseDescription"
-                      rows={5}
-                      cols={25}
-                      placeholder="Expense Description"
-                      className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
-                      value={expense.description}
-                      onChange={(e) =>
-                        onSave({ ...expense, description: e.target.value })
-                      }
-                    >
-                      Spent on Food
-                    </textarea>
-                  </div>
-                </div>
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="date"
-                    className="block text-sm/6 font-medium text-white"
-                  >
-                    Date
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="date"
-                      name="ExpenseDate"
-                      type="date"
-                      className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
-                      onChange={(e) =>
-                        onSave({ ...expense, date: new Date(e.target.value) })
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="category"
-                    className="block text-sm/6 font-medium text-white"
-                  >
-                    Category
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      id="category"
-                      name="Expense Category"
-                      value={expense.category}
-                      onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                        onSave({...expense, category: e.target.value as ExpenseCategory})
-                      }
-                    >
-                      <option value="Groceries">Groceries</option>
-                      <option value="Food">Food</option>
-                      <option value="Mobile Bill">Mobile Bill</option>
-                      <option value="Travel">Travel</option>
-                      <option value="Shopping">Shopping</option>
-                      <option value="Games">Games</option>
-                      <option value="Subscription">Subscription</option>
-                      <option value="EMI">EMI</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button
-                  type="button"
-                  onClick={onClose}
-                  className="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white hover:bg-red-400 sm:ml-3 sm:w-auto"
+    <Dialog
+      open={open}
+      as="div"
+      onClose={() => close(true)}
+      className="relative z-10 focus:outline-none"
+    >
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <div className="flex min-h-full items-center justify-center p-4">
+          <DialogPanel className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0">
+            <DialogTitle className="text-base/7 font-medium text-white">Edit Expense</DialogTitle>
+            <Description>Please make your changes below</Description>
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">Amount</Label>
+              <Input
+                type="number"
+                name="Expense Amount"
+                className="border data-focus:bg-blue-100 data-hover:shadow"
+                value={tempExpValues.amount}
+                onChange={(e) =>
+                  setTempExpValues((prev) => ({
+                    ...prev,
+                    amount: parseInt(e.target.value),
+                  }))
+                }
+              />
+              <Label className="text-sm/6 font-medium text-white">
+                Category
+              </Label>
+              <Select
+                className="border data-focus:bg-blue-100 data-hover:shadow"
+                value={tempExpValues.category}
+                onChange={(e) =>
+                  setTempExpValues((prev) => ({
+                    ...prev,
+                    category: e.target.value as ExpenseCategory,
+                  }))
+                }
+              >
+                <option value="Food">Food</option>
+                <option value="groceries">Groceries</option>
+                <option value="mobileBill">Mobile Bill</option>
+                <option value="shopping">Shopping</option>
+                <option value="games">Games</option>
+                <option value="subscription">Subscription</option>
+                <option value="emi">EMI</option>
+              </Select>
+              <ChevronDownIcon
+                className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
+                aria-hidden="true"
+              />
+              <Label className="text-sm/6 font-medium text-white">
+                Description
+              </Label>
+              <Textarea
+                className="border data-focus:bg-blue-100 data-hover:shadow"
+                rows={5}
+                cols={33}
+                value={tempExpValues.description}
+                onChange={(e) =>
+                  setTempExpValues((prev) => ({
+                    ...prev,
+                    description: e.target.value,
+                  }))
+                }
+              ></Textarea>
+              <Label className="text-sm/6 font-medium text-white">Date</Label>
+              <Input
+                type="date"
+                name="Expense Date"
+                className="border data-focus:bg-blue-100 data-hover:shadow"
+                value={tempExpValues.date.toDateString()}
+                onChange={(e) =>
+                  setTempExpValues((prev) => ({
+                    ...prev,
+                    date: new Date(e.target.value),
+                  }))
+                }
+              />
+            </Field>
+            {/* <div className="mt-4">
+                <Button
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                  onClick={saveExpense()}
                 >
                   Save
-                </button>
-                <button
-                  type="button"
-                  data-autofocus
-                  onClick={() => onClose()}
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto"
-                >
-                  Cancel
-                </button>
-              </div>
-            </DialogPanel>
-          </div>
+                </Button>
+              </div> */}
+          </DialogPanel>
         </div>
-      </Dialog>
-    </div>
+      </div>
+    </Dialog>
+    // <div>
+    //   <p>This is a modal form which will show up when a user clicks on the pen icon beside any expense, so that it let's them edit the expense and save it.</p>
+    //   <p>This modal will contain the amount field, the cateogy field, the description field, and the date field to change the date</p>
+    //   <p>And at the bottom it will have the modal's cancel and save button.</p>
+    // </div>
   );
 };
 
