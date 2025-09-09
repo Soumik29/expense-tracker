54b06ff65fcd44cd5dbfbc0163308e8556a1c5a3
Soumik Mitra <soumikdeb2@gmail.com>
2025-09-09 16:19:58 -0400

    Added some accordion functionality to the application

--- src/ExpenseCard.tsx
+++ src/ExpenseCard.tsx
@@ -1,5 +1,6 @@
 import IndividualExpense from "./IndividualExpense";
 import { type Expense } from "./types";
+import { useState } from "react";
 // The main component that wraps the list
 const ExpenseCard = (props: {
   expenseList: { [key: string]: Expense[] };
@@ -7,8 +8,13 @@ const ExpenseCard = (props: {
   onEditExpense: (expense: Expense) => void;
 }) => {
   const { expenseList, onDeleteExpense, onEditExpense } = props;
+  const [activeIndex, setActiveIndex] = useState<string | null>(null);
+  //index in this case is the date of the expense
+  const handleExpandItem = (index: string) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
   return (
-    <div className="w-full mx-auto p-6 bg-gray-900/70 backdrop-blur-xl rounded-2xl shadow-2xl border border-gray-700 h-full">
+    <div className="w-full mx-auto p-6 bg-gray-900/90 backdrop-blur-3xl rounded-2xl shadow-2xl border border-gray-700 h-full">
       {/* Header */}
       <div className="sticky top-0 z-10 p-2 mb-4 backdrop-blur-xl">
         <h1 className="flex items-center justify-center gap-3 text-3xl font-bold text-white mb-4">
@@ -24,20 +30,33 @@ const ExpenseCard = (props: {
             <p className="text-sm">Add one to get started!</p>
           </div>
         ) : (
-          Object.keys(expenseList).map((expense) => (
-            <div>
-              {expenseList[expense].map((expenses) => (
-                <div>
-                  <IndividualExpense
-                    key={expenses.id}
-                    expense={expenses}
-                    onDeleteExpense={onDeleteExpense}
-                    onEditExpense={onEditExpense}
-                  />
-                </div>
-              ))}
-            </div>
-          ))
+          Object.keys(expenseList).map((expense) => {
+            const isActiveIndex = activeIndex === expense;
+            return (
+              <div>
+                <h2
+                  onClick={() => handleExpandItem(expense)}
+                  className="bg-gray-800/50 border border-gray-700 rounded-xl shadow-lg p-5 transition-all duration-300 hover:border-blue-500 hover:shadow-blue-500/10 cursor-pointer"
+                >
+                  {expense}
+                </h2>
+                {isActiveIndex && (
+                  <div className="p-4 space-y-5">
+                    {expenseList[expense].map((expenses) => (
+                      <div key={expenses.id}>
+                        <IndividualExpense
+                          key={expenses.id}
+                          expense={expenses}
+                          onDeleteExpense={onDeleteExpense}
+                          onEditExpense={onEditExpense}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            );
+          })
         )}
       </div>
     </div>
