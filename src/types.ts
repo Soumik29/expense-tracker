886c7fef95e6d23ed693e43cb22c13ebd2acf10b
Soumik Mitra <soumikdeb2@gmail.com>
2025-09-17 16:19:51 -0400

    Changed Mobile Bill to Mobile_Bill to match the backend database schema since we can't have spaces in prisma enums

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type Expense = {
     id: number;
-    category: "Groceries" | "Food" | "Mobile Bill" | "Travel" | "Shopping" | "Games" | "Subscription" | "EMI" ;
+    category: "Groceries" | "Food" | "Mobile_Bill" | "Travel" | "Shopping" | "Games" | "Subscription" | "EMI" ;
     amount: number;
     description: string;
     date: Date;
