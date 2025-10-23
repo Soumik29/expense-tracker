4b81893cd70caa242f384d877c334f867cd1ba29
Soumik Mitra <soumikdeb2@gmail.com>
2025-10-23 00:42:19 -0400

    Made some changes in auth.controller.ts and auth.middleware.ts

--- src/backend/src/controllers/auth.controller.ts
+++ src/backend/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
 import Send from "@utils/response.utils.js";
 import { prisma } from "../db.js";
 import type { Request, Response } from "express";
+import authSchema from "../validations/auth.schema.js";
